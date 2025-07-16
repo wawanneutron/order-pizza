9933da36a8e45a4ce4b32809251fd6974542a4ab
wawanneutron <wawanneutron1331@gmail.com>
2025-07-16 15:03:13 +0700

    refactor Button component styles for improved readability

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -15,9 +15,9 @@ export default function Button({
     'inline-block rounded-full bg-red-300 font-semibold uppercase tracking-wider text-stone-800 transition-colors duration-300 hover:bg-red-500 focus:bg-red-500 focus:outline-none focus:ring focus:ring-red-500 focus:ring-offset-2 disabled:cursor-not-allowed'
 
   const sizes = {
-    md: ' px-4 py-3 md:px-6 md:py-3 text-xs md:text-base',
-    xs: ' px-3 py-2 md:px-5 md:py-3 text-xs',
-    small: ' px-2.5 py-1 md:px-3.5 md:py-2 text-sm'
+    md: 'px-4 py-3 md:px-6 md:py-3 text-xs md:text-base',
+    xs: 'px-3 py-2 md:px-5 md:py-3 text-xs',
+    small: 'px-2.5 py-1 md:px-3.5 md:py-2 text-sm'
   }
 
   const styles = {
